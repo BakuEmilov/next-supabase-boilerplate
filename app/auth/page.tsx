3c69392de01ae7f4822819abbb6a7e9d62895b4e
BakuEmilov <emilovbaku530@gmail.com>
2024-04-11 23:41:47 +0600

    added github registration

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { supabaseBrowser } from "@/lib/supabase/browser";
 import { KeyRound } from "lucide-react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 const page = () => {
+  const handleLoginWithOAuth = (provider: "github" | "google") => {
+    const supabase = supabaseBrowser();
+
+    supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: location.origin + "/auth/callback",
+      },
+    });
+  };
+
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <div className="w-96 rounded-md border p-5 space-y-5">
@@ -14,11 +28,19 @@ const page = () => {
         <p className="text-sm text-gray-300">Register/SignIn Today</p>
 
         <div className="flex flex-col gap-5">
-          <Button className="w-full flex items-center gap-2" variant="outline">
+          <Button
+            className="w-full flex items-center gap-2"
+            variant="outline"
+            onClick={() => handleLoginWithOAuth("github")}
+          >
             <FaGithub /> GitHub
           </Button>
 
-          <Button className="w-full flex items-center gap-2" variant="outline">
+          <Button
+            className="w-full flex items-center gap-2"
+            variant="outline"
+            onClick={() => handleLoginWithOAuth("google")}
+          >
             <FcGoogle /> Google
           </Button>
         </div>
